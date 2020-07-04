87d1c139d64f8390a43bab68f3dcd951f8621d70
ice1000 <ice1000kotlin@foxmail.com>
2020-07-05 03:22:29 +0800

    [ build ] Generated.java

--- meta/build.gradle.kts
+++ meta/build.gradle.kts
@@ -16,6 +16,28 @@ tasks.withType<Wrapper> {
     gradleVersion = "6.5"
 }
 
+task("copyJarDep") {
+    dependsOn(projectArend.task(":cli:copyJarDep"))
+}
+
+task("generateCode") {
+    group = "build"
+    tasks["compileJava"].dependsOn(this)
+    doLast {
+        //language=JAVA
+        val code = """
+            package org.arend.io;
+            import org.jetbrains.annotations.NotNull;
+            import java.nio.file.*;
+            public class Generated {
+              public static final @NotNull String ROOT = "${projectDir.parent.replace("\\", "\\\\")}";
+              public static final @NotNull Path ROOT_PATH = Paths.get(ROOT);
+            }
+        """.trimIndent()
+        file("src/main/java/org/arend/io/Generated.java").writeText(code)
+    }
+}
+
 repositories {
     mavenCentral()
 }
