51f70cb423de2c8d2a463d921e2b3ffbadb1349e
ice1000 <ice1000kotlin@foxmail.com>
2020-07-05 03:40:45 +0800

    [ comment ] But why?

--- meta/src/main/java/org/arend/io/IOExtension.java
+++ meta/src/main/java/org/arend/io/IOExtension.java
@@ -13,6 +13,7 @@ import org.arend.ext.module.ModulePath;
 import org.arend.ext.reference.Precedence;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 public class IOExtension extends DefaultArendExtension {
@@ -41,15 +42,26 @@ public class IOExtension extends DefaultArendExtension {
   @Override
   public void declareDefinitions(@NotNull DefinitionContributor contributor) {
     var module = ModulePath.fromString("IO.Meta");
+    var arendIoIml = "arend-io.iml";
+    var arendYaml = "arend.yaml";
     contributor.declare(module, new LongName("arendYAML"),
         "The path of file `arend.yaml`.",
-        Precedence.DEFAULT, new StringMeta(this, Generated.ROOT_PATH.resolve("arend.yaml").toString()));
+        Precedence.DEFAULT, new StringMeta(this, Generated.ROOT_PATH.resolve(arendYaml).toString()));
     contributor.declare(module, new LongName("arendIoIml"),
         "The path of file `arend-io.iml`.",
-        Precedence.DEFAULT, new StringMeta(this, Generated.ROOT_PATH.resolve("arend-io.iml").toString()));
+        Precedence.DEFAULT, new StringMeta(this, Generated.ROOT_PATH.resolve(arendIoIml).toString()));
+    contributor.declare(module, new LongName("arendYamlStr"),
+        "The string `arend.yaml`.",
+        Precedence.DEFAULT, new StringMeta(this, arendYaml));
+    contributor.declare(module, new LongName("arendIoImlStr"),
+        "The string `arend-io.iml`.",
+        Precedence.DEFAULT, new StringMeta(this, arendIoIml));
     contributor.declare(module, new LongName("projectRoot"),
         "The path to the project root.",
         Precedence.DEFAULT, new StringMeta(this, Generated.ROOT));
+    contributor.declare(module, new LongName("pathSeparator"),
+        "The path separator, works differently on each platform.",
+        Precedence.DEFAULT, new StringMeta(this, File.separator));
     contributor.declare(module, new LongName("performIO"),
         "Execute an IO action.",
         Precedence.DEFAULT, new PerformIOMeta(this));
